e75914e5e639807240b171c20bc159becea5aa63
uyennhi <uyennhinguyentruong@gmail.com>
2020-05-14 16:57:40 +0700

    design check out

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -14,35 +14,34 @@
                 <div class="col-sm-4 col-sm-offset-1">
                     <div class="login-form"><!--login form-->
                         <input type="hidden" name="_token" value="{{csrf_token()}}">
-                        <legend>Billing To</legend>
-                        <div class="form-group {{$errors->has('billing_name')?'has-error':''}}">
-                            <input type="text" class="form-control" name="billing_name" id="billing_name" value="{{$user_login->name}}" placeholder="Billing Name">
+                        <h2>Billing To</h2>
+                        <div  class="form-group {{$errors->has('billing_name')?'has-error':''}}" >
+                            <label for="male">Tên User</label>
+                            <input  type="text" class="form-control" name="billing_name" id="billing_name" value="{{$user_login->name}}" placeholder="Billing Name">
                             <span class="text-danger">{{$errors->first('billing_name')}}</span>
                         </div>
-                        <div class="form-group {{$errors->has('billing_address')?'has-error':''}}">
+                        <div class="form-group {{$errors->has('billing_address')?'has-error':''}}" >
+                            <label for="male">Địa chỉ</label>
                             <input type="text" class="form-control" value="{{$user_login->address}}" name="billing_address" id="billing_address" placeholder="Billing Address">
                             <span class="text-danger">{{$errors->first('billing_address')}}</span>
                         </div>
-                        <div class="form-group {{$errors->has('billing_city')?'has-error':''}}">
+                        <div class="form-group {{$errors->has('billing_city')?'has-error':''}}" >
+                            <label for="male">Thành Phố</label>
                             <input type="text" class="form-control" name="billing_city" value="{{$user_login->city}}" id="billing_city" placeholder="Billing City">
                             <span class="text-danger">{{$errors->first('billing_city')}}</span>
                         </div>
-                        <div class="form-group {{$errors->has('billing_state')?'has-error':''}}">
-                            <input type="text" class="form-control" name="billing_state" value="{{$user_login->state}}" id="billing_state" placeholder=" Billing State">
-                            <span class="text-danger">{{$errors->first('billing_state')}}</span>
-                        </div>
-                        <div class="form-group">
+                       
+                        <div class="form-group"  >
+                            <label for="male">Đất Nước</label>
                             <select name="billing_country" id="billing_country" class="form-control">
                                 @foreach($countries as $country)
                                     <option value="{{$country->country_name}}" {{$user_login->country==$country->country_name?' selected':''}}>{{$country->country_name}}</option>
                                 @endforeach
                             </select>
                         </div>
-                        <div class="form-group {{$errors->has('billing_pincode')?'has-error':''}}">
-                            <input type="text" class="form-control" name="billing_pincode" value="{{$user_login->pincode}}" id="billing_pincode" placeholder=" Billing Pincode">
-                            <span class="text-danger">{{$errors->first('billing_pincode')}}</span>
-                        </div>
-                        <div class="form-group {{$errors->has('billing_mobile')?'has-error':''}}">
+                        
+                        <div class="form-group {{$errors->has('billing_mobile')?'has-error':''}}" >
+                            <label for="male">Số điện thoại </label>
                             <input type="text" class="form-control" name="billing_mobile" value="{{$user_login->mobile}}" id="billing_mobile" placeholder="Billing Mobile">
                             <span class="text-danger">{{$errors->first('billing_mobile')}}</span>
                         </div>
@@ -57,35 +56,35 @@
                 </div>
                 <div class="col-sm-4">
                     <div class="signup-form"><!--sign up form-->
-                        <legend>Shipping To</legend>
+                        <h2>Shipping To</h2>
                         <div class="form-group {{$errors->has('shipping_name')?'has-error':''}}">
+                            <label for="male">Tên User</label>
                             <input type="text" class="form-control" name="shipping_name" id="shipping_name" value="" placeholder="Shipping Name">
                             <span class="text-danger">{{$errors->first('shipping_name')}}</span>
                         </div>
                         <div class="form-group {{$errors->has('shipping_address')?'has-error':''}}">
+                            <label for="male">Địa Chỉ</label>
                             <input type="text" class="form-control" value="" name="shipping_address" id="shipping_address" placeholder="Shipping Address">
                             <span class="text-danger">{{$errors->first('shipping_address')}}</span>
                         </div>
                         <div class="form-group {{$errors->has('shipping_city')?'has-error':''}}">
+                            <label for="male">Thành Phố</label>
+                            
                             <input type="text" class="form-control" name="shipping_city" value="" id="shipping_city" placeholder="Shipping City">
                             <span class="text-danger">{{$errors->first('shipping_city')}}</span>
                         </div>
-                        <div class="form-group {{$errors->has('shipping_state')?'has-error':''}}">
-                            <input type="text" class="form-control" name="shipping_state" value="" id="shipping_state" placeholder="Shipping State">
-                            <span class="text-danger">{{$errors->first('shipping_state')}}</span>
-                        </div>
+                        
                         <div class="form-group">
+                            <label for="male">Đất Nước</label>
                             <select name="shipping_country" id="shipping_country" class="form-control">
                                 @foreach($countries as $country)
                                     <option value="{{$country->country_name}}">{{$country->country_name}}</option>
                                 @endforeach
                             </select>
                         </div>
-                        <div class="form-group {{$errors->has('shipping_pincode')?'has-error':''}}">
-                            <input type="text" class="form-control" name="shipping_pincode" value="" id="shipping_pincode" placeholder="Shipping Pincode">
-                            <span class="text-danger">{{$errors->first('shipping_pincode')}}</span>
-                        </div>
+                        
                         <div class="form-group {{$errors->has('shipping_mobile')?'has-error':''}}">
+                            <label for="male">Số Điện Thoại</label>
                             <input type="text" class="form-control" name="shipping_mobile" value="" id="shipping_mobile" placeholder="Shipping Mobile">
                             <span class="text-danger">{{$errors->first('shipping_mobile')}}</span>
                         </div>
