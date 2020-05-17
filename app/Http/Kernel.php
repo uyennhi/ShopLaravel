dea538d7fb838bdc755c5b44ef9eae0dcc4b25fe
uyennhi <uyennhinguyentruong@gmail.com>
2020-05-17 16:59:48 +0700

    login admin

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\Admin::class,
         'FrontLogin_middleware' => \App\Http\Middleware\FrontLogin_middleware::class,
     ];
 }
